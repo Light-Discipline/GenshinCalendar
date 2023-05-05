cdb4b40355bd0a017de1a63af444db4f97e3dbef
Light-Discipline <1749525767@qq.com>
2023-05-05 23:34:17 +0800

    feat:培养列表系统
    0.实现了角色培养列表系统，可以将指定的角色加入或移出培养列表并保存至本地文件，之后会提醒玩家每天需要培养的角色
    1.添加了随机选择headers的部分，并将最大尝试次数提高到7，使得下载图片更加稳定
    2.查询培养材料时将一并获取并显示可以获取材料的日期
    3.增加了日历功能，可以通过选定日期来查看当天需要培养的角色

--- GUI.py
+++ GUI.py
@@ -9,123 +9,172 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from utils import getnamelist,downloadpic,search_items,filenamelist,folderexist
+from utils import getnamelist,downloadpic,search_items,filenamelist,folderexist,readcsv,writecsv,weekday,showdeveloplist
 from PyQt5.QtGui import QPixmap,QPalette
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QStringListModel
+from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QListView,QMessageBox
 import os
 
-list1=getnamelist()
-#list1=[["5-1","5-2"],["4-1","4-2","4-3"],["5-1","5-2","4-1","4-2","4-3"]]
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1339, 1059)
+        
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.gridLayout_6 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_6.setObjectName("gridLayout_6")
+        
         self.gridLayout_2 = QtWidgets.QGridLayout()
         self.gridLayout_2.setObjectName("gridLayout_2")
+        
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setMinimumSize(QtCore.QSize(96, 50))
         self.label_4.setObjectName("label_4")
+        
         self.gridLayout_2.addWidget(self.label_4, 0, 0, 1, 1)
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
+        
         self.piclabel_4 = QtWidgets.QLabel(self.centralwidget)
         self.piclabel_4.setMinimumSize(QtCore.QSize(180, 140))
         self.piclabel_4.setText("")
         self.piclabel_4.setObjectName("piclabel_4")
         self.piclabel_4.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.piclabel_4, 0, 3, 1, 1)
+        
         self.textlabel_3 = QtWidgets.QLabel(self.centralwidget)
         self.textlabel_3.setMinimumSize(QtCore.QSize(180, 50))
         self.textlabel_3.setObjectName("textlabel_3")
         self.textlabel_3.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.textlabel_3, 1, 2, 1, 1)
+        
         self.textlabel_4 = QtWidgets.QLabel(self.centralwidget)
         self.textlabel_4.setMinimumSize(QtCore.QSize(180, 50))
         self.textlabel_4.setObjectName("textlabel_4")
         self.textlabel_4.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.textlabel_4, 1, 3, 1, 1)
+        
         self.piclabel_5 = QtWidgets.QLabel(self.centralwidget)
         self.piclabel_5.setMinimumSize(QtCore.QSize(180, 140))
         self.piclabel_5.setText("")
         self.piclabel_5.setObjectName("piclabel_5")
         self.piclabel_5.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.piclabel_5, 0, 4, 1, 1)
+        
         self.textlabel_1 = QtWidgets.QLabel(self.centralwidget)
         self.textlabel_1.setMinimumSize(QtCore.QSize(180, 50))
         self.textlabel_1.setObjectName("textlabel_1")
         self.textlabel_1.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.textlabel_1, 1, 0, 1, 1)
+        
         self.textlabel_2 = QtWidgets.QLabel(self.centralwidget)
-        self.textlabel_2.setMinimumSize(QtCore.QSize(180, 50))
+        self.textlabel_2.setMinimumSize(QtCore.QSize(180, 100))
         self.textlabel_2.setObjectName("textlabel_2")
         self.textlabel_2.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.textlabel_2, 1, 1, 1, 1)
+        
         self.textlabel_5 = QtWidgets.QLabel(self.centralwidget)
         self.textlabel_5.setMinimumSize(QtCore.QSize(180, 50))
         self.textlabel_5.setObjectName("textlabel_5")
         self.textlabel_5.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.textlabel_5, 1, 4, 1, 1)
+        
         self.piclabel_1 = QtWidgets.QLabel(self.centralwidget)
         self.piclabel_1.setMinimumSize(QtCore.QSize(180, 140))
         self.piclabel_1.setText("")
         self.piclabel_1.setObjectName("piclabel_1")
         self.piclabel_1.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.piclabel_1, 0, 0, 1, 1)
+        
         self.piclabel_3 = QtWidgets.QLabel(self.centralwidget)
         self.piclabel_3.setMinimumSize(QtCore.QSize(180, 140))
         self.piclabel_3.setText("")
         self.piclabel_3.setObjectName("piclabel_3")
         self.piclabel_3.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.piclabel_3, 0, 2, 1, 1)
+        
         self.piclabel_2 = QtWidgets.QLabel(self.centralwidget)
         self.piclabel_2.setMinimumSize(QtCore.QSize(180, 140))
         self.piclabel_2.setText("")
         self.piclabel_2.setObjectName("piclabel_2")
         self.piclabel_2.setAlignment(Qt.AlignCenter)
         self.gridLayout.addWidget(self.piclabel_2, 0, 1, 1, 1)
+        
         self.gridLayout_2.addLayout(self.gridLayout, 1, 0, 1, 1)
+        
         self.gridLayout_6.addLayout(self.gridLayout_2, 3, 0, 1, 3)
+        
         self.gridLayout_4 = QtWidgets.QGridLayout()
         self.gridLayout_4.setObjectName("gridLayout_4")
+        
+        self.calendarWidget = QtWidgets.QCalendarWidget(self.centralwidget)
+        self.calendarWidget.setMaximumSize(QtCore.QSize(595, 542))
+        self.calendarWidget.setObjectName("calendarWidget")
+        self.calendarWidget.setFirstDayOfWeek(Qt.Sunday)
+        self.getdatetype()
+        self.gridLayout_6.addWidget(self.calendarWidget, 0, 1, 1, 2)
+        
+        self.developlist=readcsv('D:\\GenshinCalendar\\data.csv')
+        self.develop_name_list=[]
+        self.develop_material_list=[]
+        self.develop_day_list=[]
+        self.develop_show=[]
+        for listrow in self.developlist:
+            a=listrow.replace("\n","")
+            s=a.split(",")
+            self.develop_name_list.append(s[0])
+            self.develop_material_list.append(s[1])
+            self.develop_day_list.append(s[2])
+        for l in range(0,len(self.developlist)):
+            self.developlist[l]=[self.develop_name_list[l],self.develop_material_list[l],self.develop_day_list[l]]
+        print(self.developlist)
+        
         self.listView = QtWidgets.QListView(self.centralwidget)
+        self.listView.setEditTriggers(QtWidgets.QListView.NoEditTriggers)
         self.listView.setMinimumSize(QtCore.QSize(281, 381))
         self.listView.setObjectName("listView")
+        self.showtodaylist()
         self.gridLayout_4.addWidget(self.listView, 1, 0, 2, 1)
+        
         self.remove_button = QtWidgets.QPushButton(self.centralwidget)
         self.remove_button.setMinimumSize(QtCore.QSize(180, 50))
         self.remove_button.setObjectName("remove_button")
         self.gridLayout_4.addWidget(self.remove_button, 2, 1, 1, 1)
+        
         spacerItem = QtWidgets.QSpacerItem(20, 238, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout_4.addItem(spacerItem, 1, 1, 1, 1)
+        
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setMinimumSize(QtCore.QSize(180, 50))
         self.label_3.setObjectName("label_3")
         self.gridLayout_4.addWidget(self.label_3, 0, 0, 1, 1)
+        
         self.gridLayout_6.addLayout(self.gridLayout_4, 0, 0, 1, 1)
+        
         spacerItem1 = QtWidgets.QSpacerItem(580, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_6.addItem(spacerItem1, 2, 2, 1, 1)
+        
         spacerItem2 = QtWidgets.QSpacerItem(580, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_6.addItem(spacerItem2, 1, 2, 1, 1)
-        self.calendarWidget = QtWidgets.QCalendarWidget(self.centralwidget)
-        self.calendarWidget.setMaximumSize(QtCore.QSize(595, 542))
-        self.calendarWidget.setObjectName("calendarWidget")
-        self.gridLayout_6.addWidget(self.calendarWidget, 0, 1, 1, 2)
+        
         self.gridLayout_5 = QtWidgets.QGridLayout()
         self.gridLayout_5.setObjectName("gridLayout_5")
+        
         self.gridLayout_3 = QtWidgets.QGridLayout()
         self.gridLayout_3.setObjectName("gridLayout_3")
+        
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setMinimumSize(QtCore.QSize(108, 50))
         self.label_2.setObjectName("label_2")
         self.gridLayout_3.addWidget(self.label_2, 0, 1, 1, 1)
+        
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setMinimumSize(QtCore.QSize(108, 50))
         self.label.setObjectName("label")
         self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
+        
         self.starchoose = QtWidgets.QComboBox(self.centralwidget)
         self.starchoose.setMinimumSize(QtCore.QSize(100, 50))
         self.starchoose.setMaximumSize(QtCore.QSize(128, 50))
@@ -133,31 +182,40 @@ class Ui_MainWindow(object):
         self.starchoose.addItem("")
         self.starchoose.addItem("")
         self.gridLayout_3.addWidget(self.starchoose, 1, 0, 1, 1)
+        
+        self.list1=getnamelist()
         self.namelist = QtWidgets.QComboBox(self.centralwidget)
         self.namelist.setMinimumSize(QtCore.QSize(200, 50))
         self.namelist.setMaximumSize(QtCore.QSize(200, 50))
         self.namelist.setObjectName("namelist")
-        self.namelist.addItems(list1[0])
+        self.namelist.addItems(self.list1[0])
         self.gridLayout_3.addWidget(self.namelist, 1, 1, 1, 1)
+        
         self.gridLayout_5.addLayout(self.gridLayout_3, 0, 0, 2, 1)
+        
         spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout_5.addItem(spacerItem3, 0, 1, 1, 1)
+        
         self.add_button = QtWidgets.QPushButton(self.centralwidget)
         self.add_button.setMinimumSize(QtCore.QSize(180, 50))
         self.add_button.setMaximumSize(QtCore.QSize(180, 50))
         self.add_button.setObjectName("add_button")
         self.gridLayout_5.addWidget(self.add_button, 1, 2, 1, 1)
+        
         self.searchitems_button = QtWidgets.QPushButton(self.centralwidget)
         self.searchitems_button.setMinimumSize(QtCore.QSize(180, 50))
         self.searchitems_button.setMaximumSize(QtCore.QSize(180, 50))
         self.searchitems_button.setObjectName("searchitems_button")
         self.gridLayout_5.addWidget(self.searchitems_button, 1, 1, 1, 1)
+        
         self.gridLayout_6.addLayout(self.gridLayout_5, 1, 0, 2, 2)
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1339, 37))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+        
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -167,11 +225,13 @@ class Ui_MainWindow(object):
         
         self.starchoose.currentIndexChanged.connect(self.namelistchange)
         self.searchitems_button.clicked.connect(self.search)
-        #self.add_button.clicked.connect(self.add)
+        self.add_button.clicked.connect(self.add)
+        self.calendarWidget.clicked[QtCore.QDate].connect(self.switchdate)
+        self.remove_button.clicked.connect(self.remove)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Genshin"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "GenshinCalendar"))
         self.label_4.setText(_translate("MainWindow", "培养材料"))
         self.textlabel_3.setText(_translate("MainWindow", ""))
         self.textlabel_4.setText(_translate("MainWindow", ""))
@@ -190,20 +250,18 @@ class Ui_MainWindow(object):
     def namelistchange(self):
         self.namelist.clear()
         if(self.starchoose.currentIndex()==0):
-            self.namelist.addItems(list1[0])
+            self.namelist.addItems(self.list1[0])
         else:
-            self.namelist.addItems(list1[1])
+            self.namelist.addItems(self.list1[1])
             
-        
-    def search(self):
-        material_list=search_items(self.namelist.currentText())
-        downloadpic(material_list)
-        name=material_list[3]
+    def showpictures(self,name):
         list1=filenamelist('D:\\GenshinCalendar\\pictures\\'+name)
         itemname=list1[1]
         filename=list1[0]
         self.textlabel_1.setText(itemname[0])
-        self.textlabel_2.setText(itemname[1])
+        before=itemname[1]
+        after=before.split(";")
+        self.textlabel_2.setText(after[0]+'\n'+after[1])
         self.textlabel_3.setText(itemname[2])
         self.textlabel_4.setText(itemname[3])
         self.textlabel_5.setText(itemname[4])
@@ -213,5 +271,64 @@ class Ui_MainWindow(object):
         self.piclabel_4.setPixmap(QPixmap('D:\\GenshinCalendar\\pictures\\'+name+'./'+filename[3]))
         self.piclabel_5.setPixmap(QPixmap('D:\\GenshinCalendar\\pictures\\'+name+'./'+filename[4]))
         
-    #def add(self):
+    def search(self):
+        material_list=search_items(self.namelist.currentText())
+        downloadpic(material_list)
+        name=material_list[3]
+        self.showpictures(name)
         
+    def add(self):
+        name=self.namelist.currentText()
+        if(folderexist('D:\\GenshinCalendar\\pictures\\'+name)==0):
+            self.search()
+        else:
+            self.showpictures(name)
+        if name in self.develop_name_list:
+            print("already in the list")
+        else:
+            item_name=filenamelist('D:\\GenshinCalendar\\pictures\\'+name)[1]
+            material=item_name[1].split(";")
+            self.develop_name_list.append(name)
+            self.develop_material_list.append(material[0])
+            self.develop_day_list.append(material[1])
+            self.developlist.append([name,material[0],material[1]])
+            writecsv('D:\\GenshinCalendar\\data.csv',self.developlist)
+            self.showtodaylist()
+    
+    def getdatetype(self):
+        today=self.calendarWidget.selectedDate()
+        weekday_today=(weekday(today))
+        if(weekday_today==1 or weekday_today==4):
+            self.weekdaytype=1
+        elif(weekday_today==2 or weekday_today==5):
+            self.weekdaytype=2
+        elif(weekday_today==3 or weekday_today==6):
+            self.weekdaytype=3
+        else:
+            self.weekdaytype=4
+            
+    def showtodaylist(self):
+        self.develop_show=showdeveloplist(self.developlist,self.weekdaytype)
+        slm=QStringListModel()
+        slm.setStringList(self.develop_show)
+        self.listView.setModel(slm)
+            
+    def switchdate(self):
+        self.getdatetype()
+        self.showtodaylist()
+        
+    def remove(self):
+        try:
+            name=str(self.listView.selectedIndexes()[0].data())
+            name=name.split("----")[0]
+            for i in range(0,len(self.developlist)):
+                if(self.developlist[i][0]==name):
+                    break
+            self.developlist.pop(i)
+            self.develop_name_list.pop(i)
+            self.develop_material_list.pop(i)
+            self.develop_day_list.pop(i)
+            writecsv('D:\\GenshinCalendar\\data.csv',self.developlist)
+            self.showtodaylist()
+        except:
+            print("You haven't selected anything")
--- utils.py
+++ utils.py
@@ -2,7 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import time
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\Chrome/55.0.2883.87 Safari/537.36'}
+import csv
+from datetime import datetime
+import random
+
+headers_list = [
+        {'User-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1'}, 
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.109 Safari/537.36 CrKey/1.54.248666'},
+        {'User-agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320'},
+        {'User-agent': 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+'},
+        {'User-agent': 'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+'},
+        {'User-agent': 'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'},
+        {'User-agent': 'Mozilla/5.0 (Linux; U; Android 4.1; en-us; GT-N7100 Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'},
+        {'User-agent': 'Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G950U Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G965U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.1.0; SM-T837A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.80 Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; U; en-us; KFAPWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.13 Safari/535.19 Silk-Accelerated=true'},
+        {'User-agent': 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; LGMS323 Build/KOT49I.MS32310c) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 550) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/14.14263'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 10 Build/MOB31T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Nexus 5X Build/OPR4.170623.006) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 7.1.1; Nexus 6 Build/N6F26U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Nexus 6P Build/OPP3.170518.006) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 520)'},
+        {'User-agent': 'Mozilla/5.0 (MeeGo; NokiaN9) AppleWebKit/534.13 (KHTML, like Gecko) NokiaBrowser/8.5.0 Mobile Safari/534.13'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 9; Pixel 3 Build/PQ1A.181105.017.A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.158 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36'},
+        {'User-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'},
+        {'User-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'},
+        {'User-agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'}
+]
+
+headers = random.choice(headers_list)
+
 
 def folderexist(path):#create folder
     folder=os.path.exists(path)
@@ -16,7 +61,9 @@ def search_items(name):#search character's upgrade materials
     item_url=[]
     item_name=[]
     if(folderexist('D:\\GenshinCalendar\\pictures\\'+name)==0):#if already exisited, don't need to download again
+        print("downloading materials...")
         url="https://wiki.biligame.com/ys/"+name
+        headers = random.choice(headers_list)
         bs=BeautifulSoup(requests.get(url,headers=headers).content,"lxml")
         print("request successfully")
         i=0
@@ -47,6 +94,8 @@ def search_items(name):#search character's upgrade materials
         return [0,0,0,name]
 
 def getnamelist():#get the list of character's name
+    print('downloading namelist...')
+    time.sleep(1.5)
     url="https://wiki.biligame.com/ys/角色"
     bs=BeautifulSoup(requests.get(url,headers=headers).content,"lxml")
     star_5=[]
@@ -64,20 +113,52 @@ def getnamelist():#get the list of character's name
     print("namelist got successfully")
     return(star_5,star_4,star_all)
     
-def downloadpic(itemslist):#download the pictures of upgrade materials
-    requests.adapters.DEFAULT_RETRIES = 5
+def downloadpic(itemslist):#download the pictures of upgrade materials & get the time available
+    requests.adapters.DEFAULT_RETRIES = 7
     if(folderexist('D:\\GenshinCalendar\\pictures\\'+itemslist[3])==0):#if already exisited, don't need to download again
         pic_url=itemslist[0]
-        #item_url=itemslist[1]
+        item_url=itemslist[1]
         item_name=itemslist[2]
         name=itemslist[3]
         os.makedirs('D:\\GenshinCalendar\\pictures\\'+name)
+        print("pictures downloading...")
         for i in range(0,5):
-            with open('D:\\GenshinCalendar\\pictures\\'+name+'./%d'%(i+1)+item_name[i]+'.png', 'wb') as pic_file:
-                print("download%d/5:  "%(i+1)+pic_url[i])
+            if(i==1):#get the time that materials are available
                 time.sleep(1.5)
-                pic_file.write(requests.get(pic_url[i],headers=headers).content)
-        print("download successfully")
+                j=0
+                bs=BeautifulSoup(requests.get(item_url[1],headers=headers).content,"lxml")#get when you can find the upgrade materials
+                print('request successfully')
+                for tables in bs.find_all('table',class_='wikitable'):
+                    print('table found')
+                    for trs in tables.find_all('tr'):
+                        for ths in trs.find_all('th'):
+                            if(ths.string=='来源\n'):
+                                print("th found")
+                                j=1
+                                for tds in trs.find_all('td'):
+                                    tds=str(tds)
+                                    tds=tds.split("时间：")[1]
+                                    tds=tds.replace("/","")
+                                    tds=tds.replace("\n<td>","")
+                                    break
+                                break
+                            if(j==1):
+                                break
+                        if(j==1):
+                            break
+                with open('D:\\GenshinCalendar\\pictures\\'+name+'./%d'%(i+1)+item_name[i]+';'+tds+'.png', 'wb') as pic_file:
+                    print("download%d/5:  "%(i+1)+pic_url[i])
+                    time.sleep(1.5)
+                    headers = random.choice(headers_list)
+                    pic_file.write(requests.get(pic_url[i],headers=headers).content)
+            else:
+                with open('D:\\GenshinCalendar\\pictures\\'+name+'./%d'%(i+1)+item_name[i]+'.png', 'wb') as pic_file:
+                    print("download%d/5:  "%(i+1)+pic_url[i])
+                    time.sleep(1.5)
+                    headers = random.choice(headers_list)
+                    pic_file.write(requests.get(pic_url[i],headers=headers).content)
+        print("pictures downloaded successfully")
+        #with open('D:\\GenshinCalendar\\data.csv', "w", encoding="utf-8", newline="") as f:
         
 def filenamelist(path):
     filenamelist=os.listdir(path)
@@ -91,3 +172,40 @@ def filenamelist(path):
         namelist.append(name)
         i=i+1
     return [origin_namelist,namelist]
+
+def readcsv(path):
+    developlist1=[]
+    if(folderexist(path)==0):
+        print("Not found")
+        with open(path, "w", encoding="utf-8", newline="") as f:
+            csv_writer = csv.writer(f)
+            name=['name','material','days']
+            csv_writer.writerow(name)
+    else:
+        with open(path,'r', encoding='UTF-8') as f:
+            next(f)
+            for row in f:
+                developlist1.append(row)
+    return developlist1
+
+def writecsv(path,developlist1):
+    with open(path, "w", encoding="utf-8", newline="") as f:
+        csv_writer = csv.writer(f)
+        name=['name','material','days']
+        csv_writer.writerow(name)
+        csv_writer.writerows(developlist1)
+        
+def weekday(Qcal):
+    today=str(Qcal)
+    today=today.split('QDate(')[1]
+    today=today.replace(')','')
+    today1=datetime.strptime(today,"%Y, %m, %d")
+    weekday=datetime.date(today1).weekday()+1
+    return weekday
+
+def showdeveloplist(dlist,wdtype):
+    dlistshow=[]
+    for character in dlist:
+        if ((wdtype==1 and character[2]=='周一周四周日') or (wdtype==2 and character[2]=='周二周五周日') or (wdtype==3 and character[2]=='周三周六周日') or wdtype==4):
+            dlistshow.append(character[0]+'----'+character[1])
+    return dlistshow
